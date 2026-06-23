89f5f2c0eda9e7bcd1b45cba34744385776c9db9
Dinosh1432 <dinoshs123@gmail.com>
2026-06-23 12:51:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,13 +5,7 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "https://portfolio-shs2.vercel.app"
-  ],
-  methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type"]
-}));
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
